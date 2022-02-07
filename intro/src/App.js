835b50763c3e59e086936f4036f1547c67ed4718
Sebastian De Vita <sebassdevita@gmail.com>
2022-02-07 12:34:46 -0300

    Ejercicio componentes terminado

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,20 +1,11 @@
-import pokeball from "./img/pokeball.png"
 import "./App.css"
+import { Component } from "./components/component"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={pokeball} className="App-logo" alt="logo" />
-        <p>Intro react</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Component></Component>
       </header>
     </div>
   )
